b30202bb83a3b65cefbb1d978d3dabe81525bc82
Rajbasheer <rajbasheer79@gmail.com>
2025-03-19 12:50:52 -0400

    Automation Check

---
+++ lambda_function/lambda_function.py
@@ -0,0 +1,28 @@
+import json
+import boto3
+
+s3_client=boto3.client('s3')
+
+def lambda_handler(event, context):
+    print(f"Lambda function Updated successful!")
+
+    for record in event['Records']:
+        bucket_name= record['s3']['bucket']['name']
+        file_key = record['s3']['object']['key']
+        print(f"New file detected: {file_key} in bucket {bucket_name}")
+
+        new_key = file_key.replace("raw-data/", "processing/")
+        s3_client.copy.object(
+            Bucket=bucket_name,
+            CopySource={'Bucket': bucket_name, 'key': file_key},
+            Key=new_key
+        )
+
+        s3_client.delete_object(Bucket=bucket_name, Key=file_key)
+
+        print(f"file moved to {new_key}")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('File moved successfully')
+    }
